1c5192bfab203b44ae7b96098fa58c2f2bd46abe
unknown <greysonmrx@gmail.com>
2020-03-26 16:43:49 -0300

    feat(backend): configure mongoose connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const routes = require("./routes");
+
+mongoose.connect(
+  "mongodb+srv://aircnc:aircnc@aircnc-2jcw4.mongodb.net/tindev?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  }
+);
 
 const app = express();
 
