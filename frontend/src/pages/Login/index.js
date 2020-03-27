b2fe98c23293e76db4a470e13041f4a949110d5c
unknown <greysonmrx@gmail.com>
2020-03-27 02:24:53 -0300

    feat(frontend): fetch dev

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 import { Container } from "./styles";
+
+import api from "../../services/api";
 
 import logoImg from "../../assets/logo.svg";
 
 function Login() {
+  const [username, setUsername] = useState("");
+
+  const history = useHistory();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    try {
+      const response = await api.post("/devs", { username });
+
+      const { _id } = response.data;
+
+      history.push(`/devs/${_id}`);
+    } catch (err) {
+      alert(err.response.data.message);
+    }
+  }
+
   return (
     <Container>
-      <form>
+      <form onSubmit={handleSubmit}>
         <img src={logoImg} alt="Tindev" />
-        <input placeholder="Digite seu usuário no GitHub" />
+        <input
+          placeholder="Digite seu usuário no GitHub"
+          onChange={e => setUsername(e.target.value)}
+        />
         <button type="submit">Enviar</button>
       </form>
     </Container>
