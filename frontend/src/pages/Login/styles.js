5ce71489df669dbd379525bacf93b0b225979e08
unknown <greysonmrx@gmail.com>
2020-03-27 01:52:58 -0300

    feat(frontend): style login page

--- frontend/src/pages/Login/styles.js
+++ frontend/src/pages/Login/styles.js
@@ -1,5 +1,47 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import { darken } from "polished";
 
 export const Container = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
+  form {
+    width: 100%;
+    max-width: 300px;
+    display: flex;
+    flex-direction: column;
+
+    input {
+      margin-top: 20px;
+      border: 1px solid #ddd;
+      border-radius: 4px;
+      height: 48px;
+      padding: 0 20px;
+      font-size: 16px;
+      color: #666;
+
+      &::placeholder {
+        color: #999;
+      }
+    }
+
+    button {
+      margin-top: 10px;
+      border: 0;
+      border-radius: 4px;
+      height: 48px;
+      font-size: 16px;
+      background: #df4723;
+      font-weight: bold;
+      color: #fff;
+      cursor: pointer;
+      transition: all 0.2s;
+
+      &:hover {
+        background: ${darken(0.05, "#df4723")};
+      }
+    }
+  }
 `;
