95b0206ede1a41e44c7c64549127869a9c0253cb
unknown <greysonmrx@gmail.com>
2020-03-27 02:24:18 -0300

    feat(frontend): style main page

--- frontend/src/pages/Main/styles.js
+++ frontend/src/pages/Main/styles.js
@@ -1,9 +1,68 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  max-width: 980px;
+  margin: 0 auto;
+  padding: 50px 0;
+  text-align: center;
+`;
 
-export const List = styled.ul``;
+export const List = styled.ul`
+  list-style: none;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 30px;
+  margin-top: 50px;
+`;
 
-export const Card = styled.li``;
+export const Card = styled.li`
+  display: flex;
+  flex-direction: column;
 
-export const Buttons = styled.div``;
+  img {
+    max-width: 100%;
+    border-radius: 5px 5px 0 0;
+  }
+
+  footer {
+    flex: 1;
+    background: #fff;
+    border: 1px solid #eee;
+    padding: 15px 20px;
+    text-align: left;
+    border-radius: 0 0 5px 5px;
+
+    strong {
+      font-size: 16px;
+      color: #333;
+    }
+
+    p {
+      font-size: 14px;
+      color: #999;
+      line-height: 20px;
+      margin-top: 5px;
+    }
+  }
+`;
+
+export const Buttons = styled.div`
+  margin-top: 10px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 10px;
+
+  button {
+    height: 50px;
+    box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.05);
+    border: 0;
+    border-radius: 4px;
+    background: #fff;
+    cursor: pointer;
+
+    &:hover img {
+      transform: translateY(-5px);
+      transition: all 0.2s;
+    }
+  }
+`;
