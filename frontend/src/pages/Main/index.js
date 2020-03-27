97f175515784b5749e109aa46a36b994e71e498c
unknown <greysonmrx@gmail.com>
2020-03-27 02:32:49 -0300

    feat(frontend): fetch devs

--- frontend/src/pages/Main/index.js
+++ frontend/src/pages/Main/index.js
@@ -1,42 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import { Container, List, Card, Buttons } from "./styles";
+
+import api from "../../services/api";
 
 import logoImg from "../../assets/logo.svg";
 import likeIcon from "../../assets/like.svg";
 import dislikeIcon from "../../assets/dislike.svg";
 
-function Main() {
+function Main({ match }) {
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function loadDevs() {
+      try {
+        const response = await api.get("/devs", {
+          headers: { user: match.params.id }
+        });
+
+        setDevs(response.data);
+      } catch (err) {
+        alert(err.response.data.message);
+      }
+    }
+
+    loadDevs();
+  }, [match.params.id]);
+
   return (
     <Container>
       <Link to="/">
         <img src={logoImg} alt="Tindev" />
       </Link>
       <List>
-        <Card>
-          <img
-            src="https://avatars3.githubusercontent.com/u/43364141?v=4"
-            alt="greysonmrx"
-          />
-          <footer>
-            <strong>Greyson Filho</strong>
-            <p>
-              Full-stack programmer, fascinated by JavaScript, computer
-              technician by IFAL and currently attending the Faculty of Computer
-              Science at UFAL
-            </p>
-          </footer>
-          <Buttons>
-            <button type="button" onClick={() => {}}>
-              <img src={dislikeIcon} alt="Dislike" />
-            </button>
+        {devs.map(dev => (
+          <Card key={dev._id}>
+            <img src={dev.avatar} alt={dev.user} />
+            <footer>
+              <strong>{dev.name}</strong>
+              <p>{dev.bio}</p>
+            </footer>
+            <Buttons>
+              <button type="button" onClick={() => {}}>
+                <img src={dislikeIcon} alt="Dislike" />
+              </button>
 
-            <button type="button" onClick={() => {}}>
-              <img src={likeIcon} alt="Like" />
-            </button>
-          </Buttons>
-        </Card>
+              <button type="button" onClick={() => {}}>
+                <img src={likeIcon} alt="Like" />
+              </button>
+            </Buttons>
+          </Card>
+        ))}
       </List>
     </Container>
   );
