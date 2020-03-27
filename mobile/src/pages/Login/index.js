c0b270732d8283bd1ab259a560213d5a5a632e7c
unknown <greysonmrx@gmail.com>
2020-03-27 05:36:10 -0300

    feat(mobile): fetch dev

--- mobile/src/pages/Login/index.js
+++ mobile/src/pages/Login/index.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { Alert } from "react-native";
 
 import { Container, Img, Input, Button, ButtonText } from "./styles";
+
+import api from "../../services/api";
 
 import logoImg from "../../assets/logo.png";
 
 function Login() {
+  const [username, setUsername] = useState();
+
+  const navigation = useNavigation();
+
+  async function navigateToMain() {
+    try {
+      const response = await api.post("/devs", { username });
+
+      navigation.navigate("Main", { id: response.data._id });
+    } catch (err) {
+      Alert.alert(err.response.data.message);
+    }
+  }
+
   return (
     <Container behavior="padding">
       <Img source={logoImg} />
@@ -12,8 +30,9 @@ function Login() {
         autoCapitalize="none"
         autoCorrect={false}
         placeholder="Digite seu usuário no GitHub"
+        onChangeText={setUsername}
       />
-      <Button onPress={() => {}}>
+      <Button onPress={navigateToMain}>
         <ButtonText>Enviar</ButtonText>
       </Button>
     </Container>
